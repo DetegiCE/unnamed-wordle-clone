05dc6e3d87ad09bd21d5e86e14cd9fac2b6d897f
DetegiCE <martinok1103@naver.com>
2022-01-16 16:57:06 +0900

    add touch keyboard

--- src/keyboard.js
+++ src/keyboard.js
@@ -20,65 +20,91 @@ const getWord = () => {
 
 const word = getWord();
 
-function keyDownEvent(e) {
-    const key = e.keyCode;
-    if(key >= 65 && key <= 90) {
-        if(currentCursor === 6) {
-            return;
-        }
-        document.getElementById('w'+currentLine+currentCursor).innerText = String.fromCharCode(key);
-        currentCursor++;
+const alphaEvent = (value) => {
+    if(currentCursor === 6) {
+        return;
     }
-    else if(key === 8) { // Backspace
-        if(currentCursor === 1) {
-            return;
-        }
-        currentCursor--;
-        document.getElementById('w'+currentLine+currentCursor).innerText = '';
+    document.getElementById('w'+currentLine+currentCursor).innerText = value;
+    currentCursor++;
+}
+
+const enterEvent = () => {
+    var greenCount = 0;
+    if(currentCursor !== 6) {
+        alert("You must complete the word first!");
+        return;
     }
-    else if(key === 13) { // Enter
-        var greenCount = 0;
-        if(currentCursor !== 6) {
-            alert("You must complete the word first!");
-            return;
-        }
-        if(currentCursor === 6) {
-            if(hasWord(getWordLine(currentLine))) {
-                let colors = returnColors(word, currentLine);
-                for(var i=0 ; i<5 ; i++) {
-                    let element = document.getElementById('w'+currentLine+(i+1));
-                    console.log('w'+currentLine+i);
-                    if(colors[i] === 'G') {
-                        element.classList.add('green');
-                        greenCount++;
-                    }
-                    else if(colors[i] === 'Y') {
-                        element.classList.add('yellow');
-                    }
-                    else {
-                        element.classList.add('black');
-                    }
+    if(currentCursor === 6) {
+        if(hasWord(getWordLine(currentLine))) {
+            let colors = returnColors(word, currentLine);
+            for(var i=0 ; i<5 ; i++) {
+                let element = document.getElementById('w'+currentLine+(i+1));
+                console.log('w'+currentLine+i);
+                if(colors[i] === 'G') {
+                    element.classList.add('green');
+                    greenCount++;
+                }
+                else if(colors[i] === 'Y') {
+                    element.classList.add('yellow');
+                }
+                else {
+                    element.classList.add('black');
                 }
             }
-            else {
-                alert("Your word does not exist!");
-                return;
-            }
         }
-        currentLine++;
-        currentCursor = 1;
-        if(greenCount === 5) {
-            alert("Congratulations! You've win the game!\nRefresh for the new game.");
-            return;
-        }
-        if(currentLine === 7) {
-            alert("You lost! The answer was " + word + "\nRefresh for the new game.");
+        else {
+            alert("Your word does not exist!");
             return;
         }
     }
-    else {
+    currentLine++;
+    currentCursor = 1;
+    if(greenCount === 5) {
+        alert("Congratulations! You've win the game!\nRefresh for the new game.");
+        return;
+    }
+    if(currentLine === 7) {
+        alert("You lost! The answer was " + word + "\nRefresh for the new game.");
         return;
     }
 }
 
+const backspaceEvent = () => {
+    if(currentCursor === 1) {
+        return;
+    }
+    currentCursor--;
+    document.getElementById('w'+currentLine+currentCursor).innerText = '';
+}
+
+function keyDownEvent(e) {
+    const key = e.keyCode;
+    if(key >= 65 && key <= 90) {
+        alphaEvent(String.fromCharCode(key));
+    }
+    else if(key === 8) { // Backspace
+        backspaceEvent();
+    }
+    else if(key === 13) { // Enter
+        enterEvent();
+    }
+}
+
 document.addEventListener('keydown', keyDownEvent);
+
+var keyList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+               'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+               'U', 'V', 'W', 'X', 'Y', 'Z'];
+keyList.forEach((n) => {
+    document.getElementById('key'+n).addEventListener('click', function() {
+        alphaEvent(n);
+    })
+});
+
+document.getElementById('keyEnter').addEventListener('click', function() {
+    enterEvent();
+});
+
+document.getElementById('keyBackspace').addEventListener('click', function() {
+    backspaceEvent();
+});
